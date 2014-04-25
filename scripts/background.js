23b1df167e32fb50ca293b8a7c1c9d13006acdd9
Jack Hanford <jackhanford@gmail.com>
2014-04-25 14:24:51 -0700

    Hotkey support added

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,15 @@
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function (details) {
+chrome.commands.onCommand.addListener(function(command) {
+  console.log('onCommand event received for message: ', command);
   
+
+  var storage = chrome.storage.sync;
+
+  storage.get('group', function(data) {
+    if (data.group === undefined) { alert('No Bookmarks added yet!'); }
+
+    var groupLength = data.group.length;
+    for (var i = 0; i < groupLength; i++) {
+      chrome.tabs.create({url: data.group[i].bookmark});
+    }
+  });
 });
